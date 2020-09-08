9249005e74c7f36d66220c3a906af530a31dc08c
subinmun1997 <qzxy812@gmail.com>
2020-09-08 17:45:15 +0900

    Function

---
+++ Function.py
@@ -0,0 +1,5 @@
+def greet():
+    print("반갑습니다.")
+    print("파이썬의 세계로 오신 것을 환영합니다.")
+greet()
+greet()
---
+++ Param.py
@@ -0,0 +1,6 @@
+def greet2(name):
+    print("반갑습니다.", name)
+    print(name," 님은 파이썬의 세계로 오셨습니다.")
+
+greet2("John")
+greet2("Yoon")
