0b034cc61f7ed83a9bf8504d1cdae262ba08f49c
subinmun1997 <qzxy812@gmail.com>
2020-09-15 18:02:30 +0900

    Function

---
+++ End_Sep.py
@@ -0,0 +1,5 @@
+for i in (1,3,5,7,9):
+    print(i, end= 'm^^m')
+
+print(1,2,3,sep='_')
+print(1,2,3,sep="_",end=' m^^m ')
---
+++ Func_naming.py
@@ -0,0 +1,8 @@
+def who_are_you(name,age):
+    print("이름: ",name)
+    print("나이: ",age)
+
+who_are_you("윤성우",22)
+#who_are_you(22,"윤성우")
+who_are_you(name="윤성우",age=22)
+who_are_you(age=22,name="윤성우")
