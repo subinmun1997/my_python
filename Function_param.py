8b3b7c3d98a7f087ef879e15f2687399e006c20f
subinmun1997 <qzxy812@gmail.com>
2020-09-16 16:16:26 +0900

    Function parameter

---
+++ Function_param.py
@@ -0,0 +1,7 @@
+def func(s):
+    s[0] = 0
+    s[-1] = 0
+
+st=[1,2,3]
+func(st)
+print(st)
---
+++ List_param.py
@@ -0,0 +1,7 @@
+def add1(s):
+    for i in range(len(s)):
+        s[i]+=1
+
+st=[1,2,3,4,5]
+add1(st)
+print(st)
