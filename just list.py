f19b1c5a08081ff63080ed4ad45edffa5b81f167
subinmun1997 <qzxy812@gmail.com>
2020-09-21 23:59:16 +0900

    list comprehension

---
+++ just list.py
@@ -0,0 +1,13 @@
+r1=[1,2,3]
+r2=[]
+r3=[1,2,[3,4]]
+r4=list("Hello")
+r5=list((5,6,7))
+r6=list(range(0,5))
+
+print(r1)
+print(r2)
+print(r3)
+print(r4)
+print(r5)
+print(r6)
---
+++ twice.py
@@ -0,0 +1,6 @@
+r1=[1,2,3,4,5]
+r2=[]
+for i in r1:
+    r2.append(i*2)
+
+print(r2)
